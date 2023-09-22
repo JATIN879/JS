2f047c48da0679fcfba94d955dad701eb7d9e15b
Jatin <jatin.kumar12345w@gmail.com>
2023-09-23 00:00:54 +0530

    HighOrderLoops

---
+++ Loops_2.js
@@ -0,0 +1,44 @@
+//For of loop
+
+// ["","",""]
+// [{}, {}, {}]
+
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+   // console.log(num);
+    
+}
+
+const greetings = "Jatin Rathore";
+for (const greet of greetings) {
+    if(greet == " "){
+        continue; //for remove space in ans:
+    }
+    //console.log(`char is ${greet}`)
+    
+}
+
+
+
+
+//MAPS: ye unique value ke liye jane jaate hai:
+// const map = new Map();
+// map.set('IN',"India");
+// map.set('USA', "United States of America");
+// map.set("fr", "paris");
+// map.set('IN',"India");
+// console.log(map);
+
+// for (const [key,value] of map) {
+//      console.log(key ,':' ,value)
+// }
+
+
+
+
+
+
+
+
+
+
