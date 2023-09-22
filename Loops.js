ed7912efdabfb7519d1eaca09a6b6568f6a86427
Jatin <jatin.kumar12345w@gmail.com>
2023-09-22 23:30:50 +0530

    Loops

---
+++ Loops.js
@@ -0,0 +1,54 @@
+// console.log("welcome to loops ")
+
+// for(let i=1; i<=10; i++){
+//   console.log(`outer loop val: ${i}`);
+//   for(let j=0; j<=10; j++){
+//     //console.log(`Inner loop val ${j} and inner loop ${i}`)
+// console.log(`${i} * ${j} = ${i*j}`);  
+// }
+// }
+
+
+
+//Break-keyword
+// for (let index = 1; index < 11; index++) {
+//     if(index == 5){
+//         console.log(`5 is detected`);
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+
+
+//Continue-keyword
+// for (let index = 1; index < 11; index++) {
+//     if(index == 5){
+//         console.log(`5 is detected`);
+//         continue;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+
+
+
+
+// While and do while Iterators
+// let index =0
+// while(index <= 10){
+//     console.log(`value of index is ${index}`);
+//     index++;
+
+// }
+
+let arr =["jatin", "aman", "ankit"];
+let i =0;
+while(i < arr.length){
+    console.log(`value is arr[${i}] is ${arr[i]}`);
+    i++;
+}
+
+
